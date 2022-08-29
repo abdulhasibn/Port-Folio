67ff54cd8a4ee6c7b61d7f49b2f9ed3f4b25652a
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-29 10:43:49 +0530

    Update scroll behaviour

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const parallax = document.getElementById("welcome-section");
+const frontend = document.getElementById("frontend");
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax.style.backgroundPositionY = offset * 0.5 + "px";
+  frontend.style.PositionX = offset * 0.5 + "px";
 });
