d29fca74ee584a3388624c1646f4f1d3b0bb5a5b
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-31 11:34:07 +0530

    Change offset value for parallax effect

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const parallax = document.getElementById("welcome-section");
-const frontend = document.getElementById("frontend");
 const burger = document.getElementById("burger");
 const navlinks = document.getElementById("navlinks");
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.5 + "px";
-  frontend.style.PositionX = offset * 0.5 + "px";
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
 });
 
 burger.addEventListener("click", () => {
