780e8627eea80589ade1c7aba27eb6ed5391dfa5
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-31 11:21:46 +0530

    Make the navbar respnsive for mobiles

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const parallax = document.getElementById("welcome-section");
 const frontend = document.getElementById("frontend");
+const burger = document.getElementById("burger");
+const navlinks = document.getElementById("navlinks");
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax.style.backgroundPositionY = offset * 0.5 + "px";
   frontend.style.PositionX = offset * 0.5 + "px";
+});
+
+burger.addEventListener("click", () => {
+  navlinks.classList.toggle("nav-active");
 });
