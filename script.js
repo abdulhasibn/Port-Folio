2757ef9228f950a95088de6756402291836a61d2
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-31 12:34:22 +0530

    Chnage position of the navlinks

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const parallax = document.getElementById("welcome-section");
 const burger = document.getElementById("burger");
-const navlinks = document.getElementById("navlinks");
+const nav = document.getElementById("navlinks");
+const navlinks = document.querySelectorAll(".navlinks li");
+
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
   parallax.style.backgroundPositionY = offset * 0.7 + "px";
 });
 
 burger.addEventListener("click", () => {
-  navlinks.classList.toggle("nav-active");
+  nav.classList.toggle("nav-active");
+  // navlinks.forEach((link, index) => {
+  //   if (link.style.animation) {
+  //     link.style.animation = "";
+  //   } else {
+  //     link.style.animation = `navFadeIn .5s ease forwards ${index / 5 + 2}s`;
+  //     console.log(index / 5 + 0.5);
+  //   }
+  // });
 });
